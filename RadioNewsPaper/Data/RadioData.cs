e131b8fb160fb6a1e76aba8863ced4c906141542
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2014-09-26 22:27:05 +0530

    Packed All India Radio

--- RadioNewsPaper/Data/RadioData.cs
+++ RadioNewsPaper/Data/RadioData.cs
@@ -14,17 +14,29 @@ namespace RadioNewsPaper.Data
         }
 
         string[] radioTitle = {
-                                  "Marathi Radio",
-                                  "Radio Schizoid Chillout",
+                                  "XL Radio Gurbani",
+                                  "RadioTeenTaal",
+                                  "BOMBAY BEATS",
+                                  "Khalsa FM",
+                                  "Carnatic",
+                                  "DeSi-RaDiO",
+                                  "MAST FM",
+                                  "Akhand Path",
                               };
         string[] radioUrl = {
-                                "http://208.80.53.106:16704",
-                                "http://78.46.73.237:8000/schizoid",
+                                "http://67.228.177.153:8459/Live",
+                                "http://195.154.176.33:8000",
+                                "http://205.164.35.115:80",
+                                "http://198.178.123.8:7798",
+                                "http://67.228.150.184:7220",
+                                "http://204.45.41.148:80",
+                                "http://64.202.98.32:6210",
+                                "http://162.221.191.51:9968/akhand",
                             };
 
-        public string adBanner = "ca-app-pub-7607380003153721/7741187491";
-        public string homeInterstitial = "ca-app-pub-7607380003153721/9217920692";
-        public string detailInterstitial = "ca-app-pub-7607380003153721/1694653895";
+        public string adBanner = "ca-app-pub-7607380003153721/4927321894";
+        public string homeInterstitial = "ca-app-pub-7607380003153721/6404055094";
+        public string detailInterstitial = "ca-app-pub-7607380003153721/7880788291";
 
         public string[] returnTitle()
         {
