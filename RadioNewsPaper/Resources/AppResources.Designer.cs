2cb631fa701d6c0555ce9e25c5be116434511ca3
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2014-09-26 22:37:43 +0530

    Done packing Nigerian Radio

--- RadioNewsPaper/Resources/AppResources.Designer.cs
+++ RadioNewsPaper/Resources/AppResources.Designer.cs
@@ -79,7 +79,7 @@ namespace RadioNewsPaper.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to All India Radio Online.
+        ///   Looks up a localized string similar to Nigerian Radio.
         /// </summary>
         public static string ApplicationTitle {
             get {
